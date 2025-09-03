a6a49eb28fa873a59dad8630adc9983e407a69bd
Daiana Ibarra <daianaibarra@outlook.es>
2025-09-03 10:27:25 -0300

    Agregado TDA Pila

---
+++ include/pila.h
@@ -0,0 +1,21 @@
+#ifndef PILA_H_INCLUDED
+#define PILA_H_INCLUDED
+
+#include <stdlib.h>
+
+typedef struct sNodo{
+    void *dato;
+    unsigned tamDato;
+    struct sNodo *sig;
+}tNodo;
+typedef tNodo *tPila;
+
+void pila_crear(tPila *pila);
+int pila_llena(const tPila *pila, unsigned tamDato);
+int pila_apilar(tPila *pila, const void *dato, unsigned tamDato);
+int pila_tope(const tPila *pila, void *dato, unsigned tamDato);
+int pila_vacia(const tPila *pila);
+int pila_desapilar(tPila *pila, void *dato, unsigned tamDato);
+void pila_vaciar(tPila *pila);
+
+#endif // PILA_H_INCLUDED
---
+++ sources/pila.c
@@ -0,0 +1,100 @@
+#include <string.h>
+#include "../include/pila.h"
+#include "../include/retorno.h"
+
+#define MIN(a,b)((a) < (b) ? (a) : (b))
+
+void pila_crear(tPila *pila)
+{
+    *pila = NULL;
+}
+
+int pila_llena(const tPila *pila, unsigned tamDato)
+{
+    tNodo *nodoAux = malloc(sizeof(tNodo));
+    void *dato = malloc(tamDato);
+
+    free(nodoAux);
+    free(dato);
+
+    return nodoAux == NULL || dato == NULL;
+}
+
+int pila_apilar(tPila *pila, const void *dato, unsigned tamDato)
+{
+    tNodo *nodoNuevo = malloc(sizeof(tNodo));
+    if(!nodoNuevo){
+
+        return ERR_SIN_MEMORIA;
+    }
+
+    nodoNuevo->dato = malloc(tamDato);
+    if(!nodoNuevo->dato){
+
+        free(nodoNuevo);
+        return ERR_SIN_MEMORIA;
+    }
+
+    memcpy(nodoNuevo->dato, dato, tamDato);
+
+    nodoNuevo->tamDato = tamDato;
+    nodoNuevo->sig = *pila;
+    *pila = nodoNuevo;
+
+    return TODO_OK;
+}
+
+int pila_tope(const tPila *pila, void *dato, unsigned tamDato)
+{
+    if(*pila == NULL){
+
+        return ERR_PILA_VACIA;
+    }
+
+    memcpy(dato, (*pila)->dato, MIN(tamDato, (*pila)->tamDato));
+
+    return TODO_OK;
+}
+
+int pila_vacia(const tPila *pila)
+{
+    return *pila == NULL ? ERR_PILA_VACIA : TODO_OK;
+}
+
+int pila_desapilar(tPila *pila, void *dato, unsigned tamDato)
+{
+    tNodo *nodoAux = *pila;
+
+    if(nodoAux == NULL){
+
+        return ERR_PILA_VACIA;
+    }
+
+    *pila = nodoAux->sig;
+
+    memcpy(dato, nodoAux->dato, MIN(tamDato, nodoAux->tamDato));
+
+    free(nodoAux->dato);
+    free(nodoAux);
+
+    return TODO_OK;
+}
+
+void pila_vaciar(tPila *pila)
+{
+    tNodo *nodoAux = *pila;
+
+    while(*pila != NULL){
+
+        *pila = nodoAux->sig;
+
+        free(nodoAux->dato);
+        free(nodoAux);
+    }
+}
+
+
+
+
+
+
