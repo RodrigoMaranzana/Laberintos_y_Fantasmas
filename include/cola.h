92189ae2b1d991b47264023dcd6bebfade54e082
Rodrigo Maranzana <rodrigo-maranzana@outlook.com>
2025-09-08 15:30:40 -0300

    Correcion menor

--- include/cola.h
+++ include/cola.h
@@ -1,31 +1,17 @@
 #ifndef COLA_H_INCLUDED
 #define COLA_H_INCLUDED
-<<<<<<< HEAD
-#include "comun.h"
-=======
->>>>>>> 7f2d18524b9558ce7a63f639d92cc2f793669acc
 
+#include "comun.h"
 #define TODO_OK     0
 #define SIN_MEMORIA 1
 #define COLA_VACIA  2
 #define COLA_LLENA  3
 
-<<<<<<< HEAD
-=======
-typedef struct sNodo
-{
-    void *dato;
-    unsigned tamDato;
-    struct sNodo *sig;
-}tNodo;
-
->>>>>>> 7f2d18524b9558ce7a63f639d92cc2f793669acc
 typedef struct
 {
     tNodo *pri;
     tNodo *ult;
 }tCola;
-
 
 void cola_crear(tCola *cola);
 int cola_encolar(tCola *cola, const void *dato, unsigned tamDato);
