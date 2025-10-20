7c7029fea9a35bda8413e0d6de299c8b5e6450a2
Daiana Ibarra <daianaibarra@outlook.es>
2025-10-19 22:57:59 -0300

    Cambio de nombre del Build Target

--- include/cliente/dibujado.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIBUJADO_H_INCLUDED
-#define DIBUJADO_H_INCLUDED
-
-#include "../../include/cliente/escenario.h"
-
-void dibujado_escenario(SDL_Renderer *renderer, tEscenario *escenario, SDL_Texture **imagenes);
-
-
-#endif // DIBUJADO_H_INCLUDED
